8d319ee7aff8bdb0160c7dcf725449ec318557e4
SanketDhabarde <sanketdhabarde13@gmail.com>
2020-11-29 15:03:08 +0530

    api added

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ var translateBtn= document.querySelector("#btn-translate");
 var input = document.querySelector("#input");
 var output = document.querySelector("#output");
 
+var serverUrl="https://api.funtranslations.com/translate/minion.json";
+
+function getUrlTranslated(text)
+{
+    return serverUrl + "?" + "text=" + text;
+}
 
 translateBtn.addEventListener("click", function() {
-        output.innerText=input.value;
+        fetch(getUrlTranslated(input.value))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText=json.contents.translated;
+            output.innerText=translatedText;
+        });
   });
